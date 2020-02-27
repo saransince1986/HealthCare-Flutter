320b560105b5cc82e5a05a9ff05df6a04d026bb1
Pramish Luitel <pramish.luitel7@gmail.com>
2020-02-27 13:02:42 +1100

    changing the expected number to 10 from 1

--- test/Account_Test.dart
+++ test/Account_Test.dart
@@ -5,13 +5,13 @@ void main() {
   test('Deposit money', () {
     final account = Account();
     account.deposit(100);
-    expect(account.balance, 1);
+    expect(account.balance, 10);
   });
 
   test('Withdraw money', () {
     final account = Account();
     account.deposit(100);
     account.withdraw(10);
-    expect(account.balance, 1);
+    expect(account.balance, 10);
   });
 }
